4482cb420228e5409c6f0ba6440df571b9e181ac
Kristopher Ruzic <krruzic@gmail.com>
2019-11-19 01:57:36 -0700

    small doc changes

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,16 +1,21 @@
+// examples/simple.rs
 use rustofi::components::ItemList;
 use rustofi::RustofiResult;
 
 fn simple_app() -> RustofiResult {
+    // create a list of strings to pass as rofi options. Note that this can be any type you want,
+    // the callback will always return the type you passed in
     let rustofi_entries = vec![
         "Entry 1".to_string(),
         "Entry 2".to_string(),
         "Entry 3".to_string(),
     ];
+    // create a ItemList with a callback that prints which item was selected.
     ItemList::new(rustofi_entries, Box::new(simple_callback)).display("Select an entry".to_string())
 }
 
 pub fn simple_callback(s: &String) -> RustofiResult {
+    // when an item is clicked, print the name!
     println!("Clicked on item: {}", s);
     RustofiResult::Success
 }
@@ -18,6 +23,7 @@ pub fn simple_callback(s: &String) -> RustofiResult {
 fn main() {
     loop {
         match simple_app() {
+            // loop unless the user requests we exit
             RustofiResult::Error => break,
             RustofiResult::Exit => break,
             RustofiResult::Cancel => break,
--- src/components.rs
+++ src/components.rs
@@ -6,11 +6,45 @@
 //!
 //! ## Using ItemList
 //! This example demonstrates using the ItemList by creating a list of strings then printing off which
-//! was selected
-//! See `examples/simple.rs` or the main documentation page for code. Run with:
+//! was selected. Run with:
 //!
 //! `cargo run --example simple`
+//! ```no_run
+//! // examples/simple.rs
+//! use rustofi::components::ItemList;
+//! use rustofi::RustofiResult;
 //!
+//! fn simple_app() -> RustofiResult {
+//!     // create a list of strings to pass as rofi options. Note that this can be any type you want,
+//!     // the callback will always return the type you passed in
+//!     let rustofi_entries = vec![
+//!         "Entry 1".to_string(),
+//!         "Entry 2".to_string(),
+//!         "Entry 3".to_string(),
+//!     ];
+//!     // create a ItemList with a callback that prints which item was selected.
+//!     ItemList::new(rustofi_entries, Box::new(simple_callback)).display("Select an entry".to_string())
+//! }
+//!
+//! pub fn simple_callback(s: &String) -> RustofiResult {
+//!     // when an item is clicked, print the name!
+//!     println!("Clicked on item: {}", s);
+//!     RustofiResult::Success
+//! }
+//!
+//! fn main() {
+//!     loop {
+//!         match simple_app() {
+//!             //!  loop unless the user requests we exit
+//!             RustofiResult::Error => break,
+//!             RustofiResult::Exit => break,
+//!             RustofiResult::Cancel => break,
+//!             RustofiResult::Blank => break,
+//!             _ => {}
+//!         }
+//!     }
+//! }
+//! ```
 //! ## Using ActionList
 //! This example demonstrates using the ActionList to manipulate an object's state. As it can't
 //! return a modified item through the callback, you'll need to store your modified changes with
